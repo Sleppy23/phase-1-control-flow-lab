2de6922e5684dc5a20e9108d2539708d55733f78
Robert Sleppy <rassleppy@gmail.com>
2021-10-13 16:23:19 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
+  if(ride <= 400){
+    return "This one is on me!";
+  } else if(1999 < ride && ride < 2500){
+    return "I will gladly take your thirty bucks."
+  } else{
+    return "No can do."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
+  if(city === "NYC"){
+    return "Ok, sounds good."
+  }else{
+    return "No go."
+  }
+  
+
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(money){
+  if(money === "generous"){
+    return "Thank you so much."
+  } else if(money === "not as generous"){
+    return "Thank you."
+  }else{
+    return "Bye."
+  }
   // Write your code here!
 }
