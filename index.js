17b6756c995b61a814aa37ac0205a2fae8869944
Robert Sleppy <rassleppy@gmail.com>
2022-02-28 18:34:05 -0700

    commit tree 28a24c7cd7f302df7bc70bc0272d9e6ea5e3b1df

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function scuberGreetingForFeet(ride){
   // Write your code here!
 }
 
+
 function ternaryCheckCity(city){
   if(city === "NYC"){
     return "Ok, sounds good."
@@ -30,3 +31,37 @@ function switchOnCharmFromTip(money){
   }
   // Write your code here!
 }
+
+
+function scuberGreetingForFeet(trip){
+  if(trip <= 400){
+    return "This one is on me!"
+  }else if(1999 < trip && trip < 2500){
+    return "I will gladly take your thirty bucks."
+  }else{
+    return "No can do."
+  }
+}
+
+
+
+function ternaryCheckCity(city){
+  if(city === "NYC"){
+    return "Ok, sounds good."
+  } else{
+    return "No go."
+  }
+
+}
+
+function switchOnCharmFromTip(tip){
+if(tip === "generous"){
+  return "Thank you so much."
+}else if (tip === "not as generous"){
+  return "Thank you."
+}else{
+  return "Bye."
+}
+}
+
+console.log("testing")
